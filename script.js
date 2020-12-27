167aa9bc7d439a4871fed0bfadfb408642bd0596
Vestivian <Vestivian@gmail.com>
2020-12-27 01:37:43 -0500

    Fix: New Game feature

--- script.js
+++ script.js
@@ -3,25 +3,41 @@
 const player0ELe = document.querySelector(`.player--0`);
 const player1ELe = document.querySelector(`.player--1`);
 
-const score0Element = document.getElementById(`score--0`);
 const currentScore0El = document.getElementById(`current--0`);
 const currentScore1El = document.getElementById(`current--1`);
+
+const score0Element = document.getElementById(`score--0`);
 const score1Element = document.getElementById(`score--1`);
+
 const diceElement = document.querySelector(`.dice`);
+
 const btnNew = document.querySelector(`.btn--new`);
 const btnRoll = document.querySelector(`.btn--roll`);
 const btnHold = document.querySelector(`.btn--hold`);
 //const btnInstruct = document.querySelector //they're classes both of them
 
 //Starting Set-up
-score0Element.textContent = 0;
-score1Element.textContent = 0;
-diceElement.classList.add(`hidden`);
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0Element.textContent = 0;
+  score1Element.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+
+  diceElement.classList.add(`hidden`);
+  player0ELe.classList.remove(`player--winner`);
+  player1ELe.classList.remove(`player--winner`);
+  player0ELe.classList.add(`player--active`);
+  player1ELe.classList.remove(`player--active`);
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -72,19 +88,4 @@ btnHold.addEventListener(`click`, function () {
 
 //New Game
 
-btnNew.addEventListener(`click`, function () {
-  console.log(`New game clicked`);
-  //All scores need to reset to zero
-  scores[activePlayer] = 0; //based on the active is reset to zero?
-  currentScore = 0;
-  activePlayer = 0;
-  playing = true;
-
-  //Must hide dice and remove the player-winner status
-
-  diceElement.classList.add(`hidden`);
-  document.querySelector(`.player--${activePlayer}`).classList.remove(`player--winner`);
-  document.querySelector(`player--${activePlayer}`).classList.add(`player--active`);
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  document.getElementById(`score--${activePlayer}`).textContent = 0;
-});
+btnNew.addEventListener(`click`, init);
